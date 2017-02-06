82e06d6047df7acb94f5c2be2b677dc68eb0e06c
emma-supattapone <emmasupattapone@hanovernorwichschools.org>
2017-02-06 14:55:33 -0500

    Updated from Brython Server: 2/6/2017 2:55:49 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
 Author: Emma Supattapone
-Credit: <list sources used, if any>
+Credit: Abby Feyrer
 Assignment:
 
 Your program will ask the user the following questions, in this order:
