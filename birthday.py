8555f82aa999f5798ab2553301b159f901f73490
emma-supattapone <emmasupattapone@hanovernorwichschools.org>
2017-02-10 11:09:47 -0500

    Updated from Brython Server: 2/10/2017 11:09:46 AM

--- birthday.py
+++ birthday.py
@@ -64,7 +64,7 @@ if year < 1980:
     
 if month == "october" and day == 31:
     print("You were born on Halloween!")
-elif month == monthrn and day == todaydate:
+if month == monthrn and day == todaydate:
     print("Happy Birthday!")   
 else:
     print(name + ", you are a " + season + " baby of the " + tmp + ".")
